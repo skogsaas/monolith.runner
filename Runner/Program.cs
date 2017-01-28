210d23bad658dd64ebd5d3b68defb0d9efe52f63
Marcus Pettersen <Marcus Pettersen>
2017-01-28 09:43:23 +0100

    Initial commit

---
+++ Runner/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Skogsaas.Monolith;
+
+namespace Skogsaas.Monolith.Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Skogsaas.Monolith.Server server = new Skogsaas.Monolith.Server();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Runner/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("Skogsaas.Monolith.Runner")]
+[assembly: AssemblyDescription("Monolith")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skogsaas")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Copyright Skogsaas 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("1.0.*")]
